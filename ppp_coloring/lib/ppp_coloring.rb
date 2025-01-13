664b351ee49c0ba0c67143c5f0e957f3bfe69e65
pnpk <pnpk00@gmail.com>
2025-01-13 14:31:47 +0900

    ロジック追加

--- ppp_coloring/lib/ppp_coloring.rb
+++ ppp_coloring/lib/ppp_coloring.rb
@@ -4,5 +4,22 @@ require_relative "ppp_coloring/version"
 
 module PppColoring
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Coloring
+    TYPES = {
+      'info'      => '36',
+      'warn'      => '35',
+      'error'     => '41',
+      'success'   => '42',
+    }.freeze
+
+    def ppp(arg, type = :info)
+      puts coloring(arg, type.to_s)
+    end
+
+    def coloring(text, *a_types)
+      types_codes = a_types.map { |s| TYPES.fetch(s, s) }
+      "\e[#{types_codes.join(';')}m#{text}\e[0m"
+    end
+  end
 end
